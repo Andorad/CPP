1229a28540a8f1afc64c6e3022923dc97e022462
Noukho <gaetan.payen@ynov.com>
2024-01-08 14:58:47 +0100

    Initial commit

---
+++ test sfml/test sfml/Player_Control.h
@@ -0,0 +1,23 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+
+class Player_Control
+{
+private:
+	float xpos = 50.f;
+	float ypos = 150.f;
+	float fireRate = 0.f;
+	bool keypressedD = false;
+	bool keypressedZ = false;
+	bool keypressedQ = false;
+	bool keypressedS = false;
+	bool firing = false;
+	sf::CircleShape Game_Player;
+	sf::RenderWindow Game_Window;
+
+public:
+	Player_Control(sf::CircleShape *Player, sf::RenderWindow *Window);
+	~Player_Control();
+	void deplacement(sf::Event Game_event);
+	void fire(sf::Event Game_event);
+};
