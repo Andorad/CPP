23d6f417c925c6aea712b2299800e4610e7d7604
Andoradogaris <theo.lopez@ynov.com>
2024-01-17 12:32:21 +0100

    UI

--- test sfml/test sfml/UI.h
+++ test sfml/test sfml/UI.h
@@ -1,19 +1,21 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include <string>
 
 class UI
 {
 public:
 	UI(sf::RenderWindow& _window);
 	~UI() = default;
-	int ModifyAmmoValue(int _ammo, int modif);
-	int ModifyScoreValue(int _score, int modif);
+	void ModifyAmmoValue(int ammo);
+	void ModifyScoreValue(int score);
+	void UpdateUI(sf::RenderWindow& window);
 private:
-	int score;
-	int maxAmmoInLoader;
-	int ammo;
-	sf::Font font;
+	sf::Texture targetTexture;
+	sf::Sprite onMouseSprite;
+	sf::Vector2f spriteOffset;
 	sf::Text scoreText;
+	sf::Font font;
 	sf::Text ammoText;
 };
---
+++ test sfml/test sfml/main.cpp
@@ -0,0 +1,80 @@
+#include <SFML/Graphics.hpp>
+#include "UI.h"
+#include "toto.cpp"
+
+int score = 0;
+int maxAmmoInLoader = 10;
+int ammo = maxAmmoInLoader;
+
+void AddScore(UI& ui)
+{
+    score += 5;
+    ui.ModifyScoreValue(score);
+}
+
+void Reload(UI& ui)
+{
+    ammo += maxAmmoInLoader - ammo;
+    ui.ModifyAmmoValue(ammo);
+}
+
+void Shoot(UI& ui)
+{
+    if(ammo > 0)
+    {
+        ammo--;
+        ui.ModifyAmmoValue(ammo);
+    }
+    else
+    {
+        Reload(ui);
+    }
+}
+
+int main()
+{
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Exemple de texte");
+
+    UI ui(window);
+
+    ui.ModifyAmmoValue(ammo);
+
+    ui.ModifyScoreValue(score);
+
+    while (window.isOpen())
+    {
+        sf::Event event;
+        while (window.pollEvent(event))
+        {
+            if (event.type == sf::Event::Closed)
+            {
+                window.close();
+            }
+
+            if (event.type == sf::Event::MouseButtonPressed)
+            {
+                if (event.key.code == sf::Mouse::Left)
+                {
+                    Shoot(ui);
+                }
+            }
+             if(event.type == sf::Event::KeyPressed)
+             {
+                 if (event.key.code == sf::Keyboard::R)
+                 {
+                     Reload(ui);
+                 }
+                 else if (event.key.code == sf::Keyboard::P)
+                 {
+                     AddScore(ui);
+                 }
+             }
+        }
+
+        window.clear();
+        ui.UpdateUI(window);
+        window.display();
+    }
+
+    return 0;
+}
